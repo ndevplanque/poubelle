0e4fee9f67efe44da063e873155d78246a0bd147
Nicolas Delplanque <nicolas@delplanque.eu>
2024-06-24 17:13:00 +0200

    feat(adminPage): lets you control trash states without arduino on

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.set('view engine', 'ejs');
+
 let trashes = {
   1: {"open": false, "full": false},
   2: {"open": false, "full": false},
@@ -11,6 +13,14 @@ let trashes = {
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+function adminPage(res) {
+  res.render('admin', {trashes});
+}
+
+app.get('/admin', (req, res) => {
+  adminPage(res);
+});
+
 app.get('/trash', (req, res) => {
   res.json(trashes)
 })
@@ -19,23 +29,35 @@ app.get('/trash/:id', (req, res) => {
   res.json(trashes[req.params.id])
 })
 
-app.put('/trash/:id/open', (req, res) => {
+app.post('/trash/:id/open', (req, res) => {
   trashes[req.params.id].open = true
+  if (req.body.redirect) {
+    return adminPage(res);
+  }
   res.json(trashes[req.params.id])
 })
 
-app.put('/trash/:id/close', (req, res) => {
+app.post('/trash/:id/close', (req, res) => {
   trashes[req.params.id].open = false
+  if (req.body.redirect) {
+    return adminPage(res);
+  }
   res.json(trashes[req.params.id])
 })
 
-app.put('/trash/:id/full', (req, res) => {
+app.post('/trash/:id/full', (req, res) => {
   trashes[req.params.id].full = true
+  if (req.body.redirect) {
+    return adminPage(res);
+  }
   res.json(trashes[req.params.id])
 })
 
-app.put('/trash/:id/empty', (req, res) => {
+app.post('/trash/:id/empty', (req, res) => {
   trashes[req.params.id].full = false
+  if (req.body.redirect) {
+    return adminPage(res);
+  }
   res.json(trashes[req.params.id])
 })
 
